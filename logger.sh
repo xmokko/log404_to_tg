67821c0357fee06171c92ea4a4dc835e1e689de3
xmokko <57141338+xmokko@users.noreply.github.com>
2023-09-15 13:20:44 +0300

    set perl-regexp for grep

--- logger.sh
+++ logger.sh
@@ -4,4 +4,4 @@ TOKEN=""
 ID=""
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 
-tail -f ./access.log | grep --line-buffered -e "\s[4|5]\d\d\s\d" | while read line ; do curl -s -X POST $URL -d chat_id=$ID -d text="$line" ; done
+tail -f ./access.log | grep --line-buffered --perl-regexp -e "\s[4|5]\d\d\s\d" | while read line ; do curl -s -X POST $URL -d chat_id=$ID -d text="$line" ; done
